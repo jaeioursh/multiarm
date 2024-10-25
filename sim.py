e7a46c7a53c0e306f65f99853000d20153eb1569
jaeioursh <joshc1010@gmail.com>
2024-10-25 06:45:12 -0700

    arm updated

--- sim.py
+++ sim.py
@@ -18,8 +18,11 @@ class armsim:
 		self.reset()
 		
 	def reset(self):
-		self.d.qpos=np.array([-1.5708, -1.5708, 1.5708, -1.5708, -1.5708, 0]*3)
-		self.d.qvel=np.array([0, 0, 0, 0, 0, 0]*3)
+				#pos     quaternion
+		box_pos=[0,1,0]+[0,0,0,1]
+		box_vel=[0]*6
+		self.d.qpos=np.array(box_pos+[-1.5708, -1.5708, 1.5708, -1.5708, -1.5708, 0]*3)
+		self.d.qvel=np.array(box_vel+[0, 0, 0, 0, 0, 0]*3)
 		self.step_start=time.time()
 		return self.state()
 
@@ -27,10 +30,19 @@ class armsim:
 		return 0.0
 	
 	def state(self):
-		return self.d.qpos #self.qvel
+		pos=self.d.qpos[7:]
+		vel=self.d.qvel[6:]
+		box_pos=self.d.qpos[:7]
+		box_vel=self.d.qvel[:6]
+		box_pos=np.tile(box_pos, (3,1))
+		box_vel=np.tile(box_vel, (3,1))
+		pos=np.array(pos).reshape((3,6))
+		vel=np.array(vel).reshape((3,6))
+
+		return  np.concatenate([pos,vel,box_pos,box_vel],axis=1)
 
 	def step(self,action):
-		self.d.ctrl=np.array(action)
+		self.d.ctrl=np.array(action).flatten()
 		mujoco.mj_step(self.m, self.d)
 		if self.viewer is not None and self.viewer.is_running():
 			self.viewer.sync()
@@ -43,7 +55,9 @@ class armsim:
 
 if __name__ =="__main__":
 	sim=armsim(1)
-	state=sim.reset()
-	for i in range(1000):
-		action=[1]*18
-		state,reward=sim.step(action)
+	for i in range(30):
+		state=sim.reset()
+		for i in range(1000):
+			action=[1]*18
+			state,reward=sim.step(action)
+			print(state)
