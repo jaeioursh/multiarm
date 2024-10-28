eaa3f8688b22cf9d52cb6e3b73203855b5412c6c
jaeioursh <joshc1010@gmail.com>
2024-10-28 08:04:01 -0700

    arm with gripper added

--- sim.py
+++ sim.py
@@ -1,5 +1,5 @@
 import time
-
+import math
 import mujoco
 import mujoco.viewer
 import numpy as np
@@ -8,11 +8,12 @@ class armsim:
 	def __init__(self,view=False):
 		
 		self.m = mujoco.MjModel.from_xml_path('ur5e/multiscene.xml')
+		self.m = mujoco.MjModel.from_xml_path('ur5grip/scene.xml')
 		self.d = mujoco.MjData(self.m)
 		self.n_joints=self.m.nu
 		if view:
 			self.viewer = mujoco.viewer.launch_passive(self.m, self.d)
-			self.viewer.cam.distance = self.m.stat.extent * 7.0
+			self.viewer.cam.distance = self.m.stat.extent * 2.0
 		else:
 			self.viewer = None
 		self.reset()
@@ -21,8 +22,10 @@ class armsim:
 				#pos     quaternion
 		box_pos=[0,1,0]+[0,0,0,1]
 		box_vel=[0]*6
-		self.d.qpos=np.array(box_pos+[-1.5708, -1.5708, 1.5708, -1.5708, -1.5708, 0]*3)
-		self.d.qvel=np.array(box_vel+[0, 0, 0, 0, 0, 0]*3)
+		armpos=[-1.5708, -1.5708, 1.5708, -1.5708, -1.5708, 0] #for ar m with no gripper
+		armpos+=[0]*8 #gripper pos
+		self.d.qpos=np.array(box_pos+armpos*3)
+		self.d.qvel=np.array(box_vel+[0]*3*len(armpos))
 		self.step_start=time.time()
 		return self.state()
 
@@ -36,8 +39,8 @@ class armsim:
 		box_vel=self.d.qvel[:6]
 		box_pos=np.tile(box_pos, (3,1))
 		box_vel=np.tile(box_vel, (3,1))
-		pos=np.array(pos).reshape((3,6))
-		vel=np.array(vel).reshape((3,6))
+		pos=np.array(pos).reshape((3,6+8))
+		vel=np.array(vel).reshape((3,6+8))
 
 		return  np.concatenate([pos,vel,box_pos,box_vel],axis=1)
 
@@ -55,9 +58,12 @@ class armsim:
 
 if __name__ =="__main__":
 	sim=armsim(1)
+
 	for i in range(30):
 		state=sim.reset()
 		for i in range(1000):
 			action=[1]*18
 			state,reward=sim.step(action)
 			print(state)
+
+	
